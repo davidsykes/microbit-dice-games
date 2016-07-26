d1aba668333df2ac0cd233e6123670e2346ae8a4
David Sykes <david@sykes.org.uk>
2016-07-26 06:19:54 +0100

    starting up

---
+++ microbit.py
@@ -0,0 +1,19 @@
+
+
+class Display:
+	def scroll(self, m):
+		print 'Scroll:', m;
+	def show(self, i):
+		print 'Scroll:', i;
+
+class Image:
+	HEART = 1
+
+def sleep(t):
+	print 'sleep', t
+
+
+display = Display()
+
+
+
