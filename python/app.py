74565271ad4245ce8d783a19eb5b9f454b1316ed
David Sykes <david@sykes.org.uk>
2016-08-02 06:20:33 +0100

    Shake calls the turn function

--- python/app.py
+++ python/app.py
@@ -4,3 +4,6 @@ class App:
     def Run(self, microbit, factory):
         microbit.Image(1)
         self.gameController = factory.CreateGame(1)
+        
+    def Shake(self):
+        self.gameController.Turn()
--- python/test_app.py
+++ python/test_app.py
@@ -5,6 +5,8 @@ from app import App
 
 class TestMainApp(unittest.TestCase):
 
+    # Setup
+
     def setUp(self):
         self.app = App()
         self.mockmicrobit = MagicMock();
@@ -17,6 +19,16 @@ class TestMainApp(unittest.TestCase):
     def test_callingTheAppRunModuleRequestsGame1Controller(self):
         self.app.Run(self.mockmicrobit, self.mockfactory)
         self.mockfactory.CreateGame.assert_called_with(1)
+
+        
+    # Event triggers
+
+    def test_shakeEventCallsGameTurnFunction(self):
+        mockGame = MagicMock();
+        self.mockfactory.CreateGame = MagicMock(return_value=mockGame)
+        self.app.Run(self.mockmicrobit, self.mockfactory)
+        self.app.Shake();
+        mockGame.Turn.assert_called_with()
 
 if __name__ == '__main__':
 
